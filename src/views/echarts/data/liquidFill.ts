e8a170a89c9c4f814d3a707f3e4ec1306cd02f9e
wuyt <wuyt@sucsoft.com>
2021-03-15 16:14:36 +0800

    add echarts：水球图

--- src/views/echarts/data/liquidFill.ts
+++ src/views/echarts/data/liquidFill.ts
@@ -1,10 +1,47 @@
 //水球图
-export const liquidFill = {
-    title: {
-        text: '基本水球图'
-    },
-    series: [{
-        type: 'liquidFill',
-        data: [0.6]
-    }]
+// export const liquidFill = {
+//     title: {
+//         text: '基本水球图'
+//     },
+//     series: [{
+//         type: 'liquidFill',
+//         data: [0.6]
+//     }]
+// };
+
+export const liquidFill ={
+    series: [
+        {
+            type: 'liquidFill',
+            data: [0.1],
+            label: {
+                normal: {
+                    textStyle: {
+                        color: '#333',
+                        fontSize: 20
+                    }
+                }
+            },
+            outline: { //外圈
+                show: true,
+                borderDistance: 6,
+                itemStyle: {
+                    color: 'none',
+                    borderColor: '#86e5ac',
+                    borderWidth: 6,
+                    shadowBlur: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0)'
+                }
+            },
+            backgroundStyle: { //水球空白色
+                color: '#ffffff'
+            },
+            itemStyle: { //水球
+                color: '#86e5ac'
+            },
+            radius: '60%',
+            center: ['50%', '50%'],
+
+        }
+    ],
 };
